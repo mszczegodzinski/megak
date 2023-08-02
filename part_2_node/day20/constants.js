6372a3b7ebab633fff5e43989dccdc76210a8943
Marek Szczegodziński <mszczegodzinski@o2.pl>
2023-08-03 01:03:49 +0200

    part2_day20: encrypt and decrypt streams data

---
+++ part_2_node/day20/constants.js
@@ -0,0 +1,4 @@
+const ENCRYPTION_SALT = 'josadaoidsandsa oisa doas jA&W343799dasd[oaspojc';
+const HASH_SALT = 'dsapsjd-932uv9 hr 0urour v832ryn8m30293um0 92mupOI]r im 08u0 7F8HFS0D8FH088FH 857528';
+
+module.exports = { ENCRYPTION_SALT, HASH_SALT };
---
+++ part_2_node/day20/taskDescription.js
@@ -0,0 +1,4 @@
+/**
+ * Write stream encrypt and stream decrypt programs which they take as arguments input file name, output file name, and password. Then encrypt and decrypt the input file.
+ * Example: node streamDecrypt.js "github icon_1.png" "github icon.png" megakurs
+ */
