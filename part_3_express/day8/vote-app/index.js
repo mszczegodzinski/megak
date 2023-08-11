424e375028068f0a3aeeb5c88fcae9bae76d1b2a
Marek Szczegodziński <mszczegodzinski@o2.pl>
2023-08-11 19:08:06 +0200

    part3_day8: prevent to vote more than once from one IP address. Move routes to the new directory

--- part_3_express/day8/vote-app/index.js
+++ part_3_express/day8/vote-app/index.js
@@ -1,39 +1,20 @@
 /**
- * Step 1:
  * Create the Express app which will host our static files. On the home page we can vote for three options: yes, no, or maybe.
  * routes:
  * /vote/yes
  * /vote/no
  * /vote/check - check the result of the vote
  *
- * step 2:
+ * Make sure that each user can vote only once.
+ *
  */
 
 const express = require('express');
 const app = express();
+const { voteRouter } = require('./routes/vote');
 
 app.use(express.static('public'));
 
-let votesForYes = 0;
-let votesForNo = 0;
-let votesForMaybe = 0;
-
-app.get('/', (req, res) => {
-  res.send('You voted for Yes!');
-});
-
-app.get('/vote/yes', (req, res) => {
-  votesForYes++;
-  res.send('You voted for Yes!');
-});
-
-app.get('/vote/no', (req, res) => {
-  votesForNo++;
-  res.send('You voted for No!');
-});
-
-app.get('/vote/check', (req, res) => {
-  res.send(`There is ${votesForYes} votes for Yes, ${votesForNo} votes for No`);
-});
+app.use('/vote', voteRouter);
 
 app.listen(3000, 'localhost');
