e6e0d355d8a8e435d6f0b7e65e83090cb401e471
Marek Szczegodziński <mszczegodzinski@o2.pl>
2023-08-12 21:04:48 +0200

    part3_day10: add simple handlebars to previous app

--- part_3_express/day9/cookie-app/index.js
+++ part_3_express/day9/cookie-app/index.js
@@ -11,6 +11,7 @@
 
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const hbs = require('express-handlebars');
 const { cookieRouter } = require('./routes/cookie');
 
 const app = express();
@@ -18,6 +19,11 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+app.engine('.hbs', hbs.engine({ extname: '.hbs', layoutsDir: __dirname + '/views/layouts' }));
+app.set('view engine', '.hbs');
 app.use('/cookie', cookieRouter);
+app.get('/hi', (req, res) => {
+  res.render('home');
+});
 
 app.listen(3000, 'localhost');
