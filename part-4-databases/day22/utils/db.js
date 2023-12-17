8ad206bb9366a4d73ada177215c5fa1b2e0c7cf7
Marek Szczegodziński <mszczegodzinski@o2.pl>
2023-12-17 21:20:31 +0100

    part4_day22: refactor Repository miniproject from mysql to mongodb

---
+++ part-4-databases/day22/utils/db.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient("mongodb://localhost:27017");
+client.connect();
+
+const db = client.db("megak_todo");
+const todos = db.collection("todos");
+
+module.exports = {
+  db,
+  todos,
+  client,
+};
