7ee04eb8cf6ce9d9fc8e1a352a3be2e99376f74b
Marek Szczegodziński <mszczegodzinski@o2.pl>
2023-12-24 00:34:16 +0100

    part5_day17: add basic project structure, assets, replace commonjs imports and exports by esmodules

---
+++ part-5-typescript/day17-18/index.ts
@@ -0,0 +1,104 @@
+import * as express from "express";
+import * as cookieParser from "cookie-parser";
+import { engine } from "express-handlebars";
+import { ConfiguratorRouter } from "./routes/configurator";
+// import { HomeRouter } from "./routes/home";
+// import { OrderRouter } from "./routes/order";
+import { handlebarsHelpers } from "./utils/handlebars-helpers";
+import { COOKIE_BASES, COOKIE_ADDONS } from "./data/cookies-data";
+import { Application, json, Response, Request } from "express";
+import { Entries } from "./types/entries";
+
+export class CookieMakerApp {
+  private app: Application;
+  private data = {
+    COOKIE_BASES,
+    COOKIE_ADDONS,
+  };
+  constructor() {
+    this._configureApp();
+    this._setRoutes();
+    this._run();
+  }
+
+  _configureApp() {
+    this.app = express();
+
+    this.app.use(json());
+    this.app.use(express.static("public"));
+    this.app.use(cookieParser());
+    this.app.engine(
+      ".hbs",
+      engine({
+        extname: ".hbs",
+        helpers: handlebarsHelpers,
+      }),
+    );
+    this.app.set("view engine", ".hbs");
+  }
+
+  _setRoutes() {
+    // this.app.use("/", new HomeRouter(this).router);
+    this.app.use("/configurator", new ConfiguratorRouter(this).router);
+    // this.app.use("/order", new OrderRouter(this).router);
+  }
+
+  _run() {
+    this.app.listen(3000, "0.0.0.0", () => {
+      console.log("Listening on http://localhost:3000");
+    });
+  }
+
+  showErrorPage(res: Response, description: string) {
+    res.render("error", {
+      description,
+    });
+  }
+
+  getAddonsFromReq(req: Request) {
+    const { cookieAddons } = req.cookies as {
+      cookieAddons: string;
+    };
+    return cookieAddons ? JSON.parse(cookieAddons) : [];
+  }
+
+  getCookieSettings(req: Request): {
+    addons: string[];
+    base: string | undefined;
+    sum: number;
+    allBases: Entries;
+    allAddons: Entries;
+  } {
+    const { cookieBase: base } = req.cookies as {
+      cookieBase?: string;
+    };
+
+    const addons = this.getAddonsFromReq(req);
+
+    const allBases = Object.entries(this.data.COOKIE_BASES);
+    const allAddons = Object.entries(this.data.COOKIE_ADDONS);
+
+    const sum =
+      (base ? handlebarsHelpers.findPrice(allBases, base) : 0) +
+      addons.reduce(
+        (prev: number, curr: string) =>
+          prev + handlebarsHelpers.findPrice(allAddons, curr),
+        0,
+      );
+
+    return {
+      // Selected stuff
+      addons,
+      base,
+
+      // Calculations
+      sum,
+
+      // All possibilities
+      allBases,
+      allAddons,
+    };
+  }
+}
+
+new CookieMakerApp();
---
+++ part-5-typescript/day17-18/routes/order.ts
@@ -0,0 +1,40 @@
+import * as express from "express";
+
+export class OrderRouter {
+  constructor(cmapp) {
+    this.cmapp = cmapp;
+    this.router = express.Router();
+    this.setUpRoutes();
+  }
+
+  setUpRoutes() {
+    this.router.get("/summary", this.sumary);
+    this.router.get("/thanks", this.thanks);
+  }
+
+  sumary = (req, res) => {
+    const { sum, addons, base, allBases, allAddons } =
+      this.cmapp.getCookieSettings(req);
+
+    res.render("order/summary", {
+      cookie: {
+        base,
+        addons,
+      },
+      allBases,
+      allAddons,
+      sum,
+    });
+  };
+
+  thanks = (req, res) => {
+    const { sum } = this.cmapp.getCookieSettings(req);
+
+    res
+      .clearCookie("cookieBase")
+      .clearCookie("cookieAddons")
+      .render("order/thanks", {
+        sum,
+      });
+  };
+}
