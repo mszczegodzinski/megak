625bd30cdbc09ef422e781caeecf2068dabe58dc
Marek Szczegodziński <mszczegodzinski@o2.pl>
2024-01-05 21:17:51 +0100

    part5_day23-24: add warrior registration confirmation

--- part-5-typescript/day21-25/routers/warrior.ts
+++ part-5-typescript/day21-25/routers/warrior.ts
@@ -1,11 +1,30 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { WarriorRecord } from '../records/warrior.record';
+import { ValidationError } from '../utils/errors';
 
 export const warriorRouter = Router();
 
 warriorRouter
-  .get("/register", (req, res) => {
-    res.render("views/register.hbs", {});
+  .get('/add-form', (req, res) => {
+    res.render('warrior/add-form', {});
   })
-  .post("/", (req, res) => {
-    res.render("views/ranking.hbs", {});
+  .post('/', async (req, res) => {
+    const { power, defense, stamina, agility, name } = req.body;
+    const warrior = new WarriorRecord({
+      ...req.body,
+      power: Number(power),
+      defense: Number(defense),
+      stamina: Number(stamina),
+      agility: Number(agility),
+    });
+
+    if (await WarriorRecord.isNameTaken(name)) {
+      throw new ValidationError(`Imię ${name} jest zajęte! Wybierz inne!`);
+    } else {
+      const id = await warrior.insert();
+      res.render('warrior/warrior-added.hbs', {
+        name: warrior.name,
+        id,
+      });
+    }
   });
